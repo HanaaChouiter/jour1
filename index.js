9278ffb59fa0885020ab0dcf06c7827ae58d0991
HanaaChouiter <hanaa7chouiter@gmail.com>
2021-09-27 15:35:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+//console.log("Hello World !");
+
+var test = "My name is Bruce";
+//console.log(test)
+
+var firstName = "Wayne";
+//console.log("Nice to meet you " + firstName)
+
+var testLength = "I'm very long !";
+//console.log(testLength.length)
+
+var food = "croissant is meh";
+//console.log(food.replace("meh", "so good"))
+
+var basic = "This is Cool";
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLowerCase();
+//console.log(basicUp)
+//console.log(basicDown)
+
+var word="banana";
+var letters= word.split("") ;
+//console.log(letters)
+
+var age = 23;
+var template = `I m ${age} years old`;
+//console.log(template)
+
+var test = "Bruce\nWayne"
+//console.log(test)
+
+var changed = "Bonjour";
+var changed= changed.replaceAll("o","a")
+console.log(changed)
+
+
